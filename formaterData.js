34f620aa364e1073c5d5e71980a3070bb03cdaef
Franco Martin Colombo <colombo.developer@hotmail.com>
2021-11-06 01:39:42 -0300

    server-meli create

---
+++ formaterData.js
@@ -0,0 +1,65 @@
+const author = {
+  name: "Franco Martin",
+  lastname: "Colombo",
+};
+
+const formatPrice = (currency, price) => {
+  if (/\./.test(String(price))) {
+    return {
+      currency,
+      amount: Number(String(price).split(".")[0]),
+      decimals: Number(String(price).split(".")[1]),
+    };
+  } else {
+    return {
+      currency,
+      amount: price,
+      decimals: 0,
+    };
+  }
+};
+
+const formatPicture = (url) => url.replace("http", "https");
+
+const formatItems = ({ available_filters, filters, results }) => {
+  const categories = () => {
+    let categoriesFromAvailableFilters = available_filters.filter(
+      (filter) => filter.id === "category"
+    );
+    let categoriesFromFilters = filters.filter(
+      (filter) => filter.id === "category"
+    );
+    if (categoriesFromAvailableFilters.length > 0) {
+      return categoriesFromAvailableFilters[0].values.map(({ name }) => name);
+    } else if (categoriesFromFilters.length > 0) {
+      return categoriesFromFilters[0].values.map(({ name }) => name);
+    }
+  };
+
+  const items = results.map((result) => ({
+    id: result.id,
+    title: result.title,
+    price: formatPrice(result.currency_id, result.price),
+    picture: formatPicture(result.thumbnail),
+    condition: result.condition,
+    free_shipping: result.shipping.free_shipping,
+  }));
+  return { author, categories: categories(), items };
+};
+
+const formatItem = (product, description) => {
+  const item = {
+    id: product.id,
+    title: product.title,
+    price: formatPrice(product.currency_id, product.price),
+    picture: formatPicture(product.pictures[0].url),
+    condition: product.condition,
+    free_shipping: product.shipping.free_shipping,
+    sold_quantity: product.sold_quantity,
+    description: description[0].plain_text,
+  };
+
+  return { author, item };
+};
+
+module.exports = { formatItems, formatItem };
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const proxy = "https://api.mercadolibre.com";
+const { formatItem, formatItems } = require("./formaterData");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+app.use(function (req, res, next) {
+  var allowedOrigins = ["http://localhost:3000"];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("bienvenido al server de Meli");
+});
+
+app.get("/api/items", async ({ query: { q } }, res) => {
+  try {
+    const response = await fetch(`${proxy}/sites/MLA/search?q=${q}&limit=4`);
+    const data = await response.json();
+
+    if (data.results.length === 0) {
+      res.status(204).send(`No se obtubieron resultados de ${q}`);
+    } else {
+      const items = formatItems(data);
+      res.json(items);
+    }
+  } catch (error) {
+    res.status(404).send("No se pudo obtener los datos de Mercado Libre");
+  }
+});
+
+app.get("/api/items/:id", async (req, res) => {
+  try {
+    const [productResponse, descriptionResponse] = await Promise.all([
+      fetch(`${proxy}/items/${req.params.id}`),
+      fetch(`${proxy}/items/${req.params.id}/descriptions`),
+    ]);
+
+    const productData = await productResponse.json();
+    const descriptionData = await descriptionResponse.json();
+
+    const item = formatItem(productData, descriptionData);
+    res.json(item);
+  } catch {
+    res.status(404).send("No se pudo obtener los datos de Mercado Libre");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
